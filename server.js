669ea088bd81aa3d3864e380318093905a37b3d8
davezer <davezer27@gmail.com>
2021-12-05 18:21:33 -0500

    added front end integration

--- server.js
+++ server.js
@@ -4,11 +4,14 @@ const path = require('path');
 const express = require('express');
 const { animals } = require('./data/animals.json');
 const { type } = require('os');
+const { response } = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
+
 
 
 function filterByQuery(query, animalsArray) {
@@ -111,6 +114,22 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'))
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
